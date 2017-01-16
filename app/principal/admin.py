8f6ee054df695a44542ae7838a29c8c9724a8cbb
Walter Lemus <walter.lemus@hotmail.com>
2017-01-16 01:53:47 -0600

    agregando elementos a la app

--- app/principal/admin.py
+++ app/principal/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from app.principal.models import *
 
 # Register your models here.
+
+admin.site.register(codigo)
+admin.site.register(kardex)
--- app/principal/models.py
+++ app/principal/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Codigo(models.Model):
+    codigo = models.CharField(max_length=10, primary_key=True)
+    nombre = models.CharField(max_length=500)
+
+    def __unicode__(self):
+        return '{} {}'.format(self.codigo, self.nombre)
+
+class Kardex(models.Model):
+	codigo = models.ForeignKey(Codigo, null=False, blank=False, on_delete=models.CASCADE)
+	fecha = models.DateField()
+	concepto = models.CharField(max_length=100)
+	entrada = models.IntegerField()
+	precio_entrada = models.FloatField(null=True, blank=True)
+	salida = models.IntegerField()
+	precio_salida = models.FloatField(null=True,blank=True)
+	saldo = models.FloatField()
+
+	def __unicode__(self):
+        return '{}'.format(self.codigo)
+
+
